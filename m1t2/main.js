4e2c98eab4c39038a6b76863ff63aedb3f4a0623
“SergeSadowsky” <root@levsha.local>
2023-05-15 18:23:20 +0300

    Task for M1 Thema 2 finished.

---
+++ m1t2/main.js
@@ -0,0 +1,27 @@
+"use strict";
+{
+  let title = "Technics SL-1200GR";
+  let quantity = 33;
+  let category = "Turntables";
+  let price = 1200;
+
+  console.log(`Наименование: ${title}`);
+
+  // additional
+  let total = quantity * price;
+  console.log(`Общая сумма: ${total}`);
+}
+
+
+{
+  let title = "Marantz PM6007";
+  let quantity = 3;
+  let category = "Integrated Amplifiers";
+  let price = 2200;
+
+  console.log(`Наименование: ${title}`);
+
+  // additional
+  let total = quantity * price;
+  console.log(`Общая сумма: ${total}`);
+}
