3b34d709806e0b969616fbf5a8bb41edfb13a2aa
“SergeSadowsky” <root@levsha.local>
2023-05-15 18:01:19 +0300

    Initial commit

---
+++ m1t1/main.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let title = "Technics SL-1200GR"
+let quantity = 33;
+let category = "Turntables";
+let price = 1200;
+
+console.log(`Наименование: ${title}`);
+
+// additional
+let total = quantity*price;
+console.log(`Общая сумма: ${total}`);
