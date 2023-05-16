77de29a18ce47fa9985b03dd688b0c0181b04a25
SergeSadowsky <developer@sadowsky.spb.ru>
2023-05-16 23:52:59 +0300

    Finished m1t3

---
+++ m1t3/einstein.js
@@ -0,0 +1,4 @@
+const myWeight = 100;
+const speedLight = 3e8;
+const result = myWeight * speedLight ** 2;
+console.log(`Энергия равна ${result} Дж`)
--- m1t3/main.js
+++ m1t3/main.js
@@ -25,3 +25,18 @@
   let total = quantity * price;
   console.log(`Общая сумма: ${total}`);
 }
+
+{
+  const title = prompt("Введите наименование товара: ");
+  const quantity = Number.parseFloat(prompt("Введите кол-во: ").trim());
+  const category = prompt("Введите категорию товара: ");
+  const price = Number.parseFloat(prompt("Введите стоимость: ").trim());
+
+  console.log(`${title} - ${typeof title}`);
+  console.log(`${quantity} - ${typeof quantity}`);
+  console.log(`${category} - ${typeof category}`);
+  console.log(`${price} - ${typeof price}`);
+
+  console.log(`на складе ${quantity} еденицы товара ${title} на сумму ${quantity*price} оловянных`)
+  
+}
