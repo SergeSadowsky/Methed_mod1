86e1e9af6090493454fa9b1e7facf736949a10ac
SergeSadowsky <developer@sadowsky.spb.ru>
2023-05-19 16:45:32 +0300

    m1t5 finished

--- m1t5/Task04.js
+++ m1t5/Task04.js
@@ -1,7 +1,21 @@
 const calculate = (sum, quantity, promo='') => {
+    let total = sum;
 
+    if(quantity > 10) total *= 0.97;
+    if(total > 30000) {
+        total = 30000 + (total - 30000) * 0.85;
+    }
+
+    if(promo){
+        if(promo==='METHED') total *= 0.9;
+        if(promo==='G3H2Z1') {
+            if ( total > 2000) total -=500; 
+        }
+    }
+    return total;
 }
-
 console.log(calculate(4500,10,''));
-console.log(calculate(4500,10,'METHED'));
-console.log(calculate(4500,10,'G3H2Z1'));
+console.log(calculate(4500,11,''));
+console.log(calculate(45000,11,''));
+console.log(calculate(45000,10,'METHED'));
+console.log(calculate(45000,11,'G3H2Z1'));
