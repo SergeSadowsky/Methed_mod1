df342d46af5979a4f889d96f589df1713478e8d6
SergeSadowsky <developer@sadowsky.spb.ru>
2023-05-19 20:32:47 +0300

    Finished m1 t6

--- m1t6/Task01.js
+++ m1t6/Task01.js
@@ -1,3 +1,19 @@
 // Выведите в консоль таблицу степеней от 1 до 10 по аналогии как в уроке выводится таблица умножения
 //
 
+{
+    const arr = [];
+    for(let i=1; i<=10; i++)
+    {
+        let str = '';
+        let subArray = [];
+
+        for(let j=1; j<=10; j++){
+            str += (i**j).toString().padStart(12)
+            subArray.push(i**j);
+        }
+        console.log(str);
+        arr.push(subArray);
+    }
+    console.table(arr);
+}
--- m1t6/Task02.js
+++ m1t6/Task02.js
@@ -2,4 +2,22 @@
 // Она принимает число и возвращает true, если число является простым, а в ином случае false. 
 //
 
+const isPrime = x => {
+    let set = new Set([1,2,3,5,7,11]);
+    if(set.has(x)) return true;
+    if(x > 11){
+        if(x % 2 == 0 || x % 5 == 0) return false;
+
+        for(let i=3;i*i<=x;i++)
+            if(x%i === 0)
+                return false;
+        return true;
+    }
+    return false;    
+}
+
+for(let i=1; i<=2000; i++) { console.log(`${i}: ${isPrime(i)}`) };
+
+
+
 
