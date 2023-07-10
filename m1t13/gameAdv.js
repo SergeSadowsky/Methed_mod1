85a66d3511c66271a01031b14c45663cb841a0db
SergeSadowsky <developer@sadowsky.spb.ru>
2023-07-10 12:03:36 +0300

    m1 t13 stage 1

--- m1t13/gameAdv.js
+++ m1t13/gameAdv.js
@@ -1,9 +1,33 @@
 'use strict';
 
-window.rsp = (function() {
+(function() {
+  const FIGURES_ENG = ['rock', 'scissors', 'paper'];
+  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
 
-  return 'rrr';
+  const getRandomInInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const getFigure = (lang) => {
+
+  };
+
+  const game = (language) => {
+    const result = {
+      player: 0,
+      computer: 0,
+    };
+
+    const lang = language === 'EN' || language === 'ENG' ?
+      FIGURES_ENG : FIGURES_RUS;
+
+    return function start() {
+
+    };
+
+  };
+
+  window.gameRPS = game;
 })();
-
-
-console.log(window.rsp);
