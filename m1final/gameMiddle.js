2f2dcffb98db883dd550546e7650a39415af8d62
SergeSadowsky <developer@sadowsky.spb.ru>
2023-07-14 02:24:07 +0300

    m1 final level middle finished

--- m1final/gameMiddle.js
+++ m1final/gameMiddle.js
@@ -3,29 +3,76 @@
 (function() {
   const ANSWERS = ['чёт', 'нечёт'];
 
-  const getBotNumber = (botBalls) => Math.floor(Math.random() * botBalls + 1);
+  const getBotNumber = (botBalls) => Math.floor(Math.random() *
+    Math.min(botBalls, 10 - botBalls) + 1);
 
   const getBotAnswer = (botBalls) => {
     if (10 - botBalls === 1) return 1;
     return Math.floor(Math.random() * 100) % 2;
   };
 
-  const getUserNumber = (max) => {
-    const input = prompt(`Загадайте целое число от 1 до ${max}`);
+  const getUserNumber = (playerBalls) => {
+    let input = prompt(`Загадайте целое число от 1 до ${playerBalls}`);
     if (!Number.isInteger(parseInt(input))) {
-      return getNumber(max);
+      return getUserNumber(playerBalls);
     }
-    if ((+input) < 1 || (+input) > max) return getNumber(max);
-    return +input;
+    input = +input;
+    if (input < 1 || input > playerBalls) {
+      return getUserNumber(playerBalls);
+    }
+    return input;
   };
 
-  const getUserAnswer = (max) => {
-    const input = prompt(`Загадайте целое число от 1 до ${max}`);
-    if (!Number.isInteger(parseInt(input))) {
-      return getNumber(max);
+  const getUserAnswer = (playerBalls) => {
+    const input = prompt(`${ANSWERS.join(', ')}?`);
+    const index = ANSWERS.findIndex(element => element.toLowerCase()
+      .startsWith(input.toLowerCase()));
+    if (index === -1) {
+      return getUserAnswer();
     }
-    if ((+input) < 1 || (+input) > max) return getNumber(max);
-    return +input;
+    return index;
+  };
+
+  const userMove = (balls) => {
+    const playerNum = getUserNumber(balls.player);
+    const botAnswer = getBotAnswer(balls.computer);
+    // eslint-disable-next-line max-len
+    console.log(playerNum, botAnswer % 2 ? 'Нечёт' : 'Чёт');
+    if (playerNum % 2 === botAnswer) {
+      balls.player -= playerNum;
+      balls.computer += playerNum;
+    } else {
+      balls.computer -= playerNum;
+      balls.player += playerNum;
+    }
+    return botAnswer % 2 ? 'Нечёт' : 'Чёт';
+  };
+
+  const botMove = (balls) => {
+    const botNum = getBotNumber(balls.computer);
+    const playerAnswer = getUserAnswer(balls.player);
+    // eslint-disable-next-line max-len
+    console.log(playerAnswer % 2 ? 'Нечёт' : 'Чёт', botNum);
+    if (botNum % 2 === playerAnswer) {
+      balls.player += botNum;
+      balls.computer -= botNum;
+    } else {
+      balls.computer += botNum;
+      balls.player -= botNum;
+    }
+    return playerAnswer % 2 ? 'Нечёт' : 'Чёт';
+  };
+
+  const isGameOver = (balls) => {
+    if (balls.player < 1) {
+      alert('Вы проиграли!');
+      return true;
+    }
+    if (balls.computer < 1) {
+      alert('Вы выиграли!');
+      return true;
+    }
+    return false;
   };
 
   const game = () => {
@@ -34,29 +81,21 @@
       computer: 5,
     };
 
+    // eslint-disable-next-line max-len
     alert(`Старт \n\nКоличество шариков:\nИгрок - ${balls.player}, Бот - ${balls.computer}`);
 
     return function start() {
-      const playerNum = getNumber(balls.player);
-      const botNum = botAnswer(balls.computer);
-      const botOddEven = botNum % 2 ? 'Нечёт' : 'Чёт';
-      alert(`Бот: ${botOddEven}`);
-      if (playerNum % 2 === botNum) {
-        balls.player -= playerNum;
-        balls.computer += playerNum;
-        if (balls.player < 1) {
-          alert('Вы проиграли!');
-          return;
-        }
-      } else {
-        balls.computer -= playerNum;
-        balls.player += playerNum;
-        if (balls.computer < 1) {
-          alert('Вы выиграли!');
-          return;
-        }
-      }
-      alert(`Количество шариков:\nИгрок - ${balls.player}, Бот - ${balls.computer}`);
+      let oddEven = userMove(balls);
+      if (isGameOver(balls)) return;
+      // eslint-disable-next-line max-len
+      alert(`Бот: ${oddEven}\nКоличество шариков:\nИгрок - ${balls.player}, Бот - ${balls.computer}`);
+
+      alert('Бот загадал число.');
+      oddEven = botMove(balls);
+      if (isGameOver(balls)) return;
+      // eslint-disable-next-line max-len
+      alert(`Ваш ответ был: ${oddEven}\nКоличество шариков:\nИгрок - ${balls.player}, Бот - ${balls.computer}`);
+
       start();
     };
   };
